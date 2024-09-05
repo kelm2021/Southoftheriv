a0e6ec78d0df16d36eb1ad1402b305b19b190a17
kelm2021 <119882399+kelm2021@users.noreply.github.com>
2024-09-05 13:07:17 -0700

    Update +page.svelte

--- src/routes/matchups/+page.svelte
+++ src/routes/matchups/+page.svelte
@@ -15,3 +15,4 @@
 <div id="main">
     <MatchupsAndBrackets {queryWeek} {matchupsData} {bracketsData} {playersData} {leagueTeamManagersData} />
 </div>
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR_L2uJOH9jXVvouDI587NFSo8d4V6oxCkWSZ-9UTAAjuuL6pQRQg4z40vEkApRBAuteCTD9vvlub5J/pubhtml?gid=1459315646&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
