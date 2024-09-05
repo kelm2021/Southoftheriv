9c5d95d1d113e4c85699e7d986f962aad925bcc6
kelm2021 <119882399+kelm2021@users.noreply.github.com>
2024-09-05 13:02:56 -0700

    Update +page.svelte

--- src/routes/standings/+page.svelte
+++ src/routes/standings/+page.svelte
@@ -16,3 +16,7 @@
 <div class="holder">
 	<Standings {standingsData} {leagueTeamManagersData} />
 </div>
+
+<div class="holder">
+	<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR_L2uJOH9jXVvouDI587NFSo8d4V6oxCkWSZ-9UTAAjuuL6pQRQg4z40vEkApRBAuteCTD9vvlub5J/pubhtml?gid=1459315646&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+</div>
