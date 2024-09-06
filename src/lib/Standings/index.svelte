fffb000a9be61c191da47a088f04c8f35f308726
kelm2021 <119882399+kelm2021@users.noreply.github.com>
2024-09-05 17:42:41 -0700

    Update index.svelte

--- src/lib/Standings/index.svelte
+++ src/lib/Standings/index.svelte
@@ -121,4 +121,4 @@
         </DataTable>
     </div>
 {/if}
-<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR_L2uJOH9jXVvouDI587NFSo8d4V6oxCkWSZ-9UTAAjuuL6pQRQg4z40vEkApRBAuteCTD9vvlub5J/pubhtml?widget=true&amp;headers=false" width="900" height="450" frameborder="0" marginheight="0" marginwidth="0"></iframe>
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR_L2uJOH9jXVvouDI587NFSo8d4V6oxCkWSZ-9UTAAjuuL6pQRQg4z40vEkApRBAuteCTD9vvlub5J/pubhtml?widget=true&amp;headers=false" width="500" height="400" frameborder="0" marginheight="0" marginwidth="0"></iframe>
